365578ae0cb893f6dd0553f8565ca508c9374ebd
krishchapla <krishc.gwis@gmail.com>
2025-07-30 09:02:18 +0530

    Commiting for updation

---
+++ src/Main.kt
@@ -0,0 +1,17 @@
+import com.sun.source.doctree.SummaryTree
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+fun main(args: Array<String>) {
+    val int:Int =207
+    val float:Float =20F
+    val double:Double =200.0
+    val long:Long =2003139301L
+    val short:Short =-2
+    val char:Char ='a'
+    val byte:Byte =127
+    val bool:Boolean =true
+    val string:String ="Hello World"
+    println("$string \n$float \n$double \n$long \n$short \n$char \n$byte \n$bool ")
+
+}
---
+++ src/pr1_3.kt
@@ -0,0 +1,18 @@
+fun main() {
+    println("Enrollment Number: ")
+    var a: Int = readLine()!!.toInt()
+    println("Student name:")
+    var b = readLine()
+    println("Student Branch:")
+    var c = readLine()
+    println("Student class:")
+    var d = readLine()
+    println("Student Batch:")
+    var e: String= readLine()!!.toString()
+    println("College name:")
+    var f: String= readLine()!!.toString()
+    println("Univerity name:")
+    var g: String= readLine()!!.toString()
+
+
+}
